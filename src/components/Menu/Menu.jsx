3ff293407b02f833ee939b1160a6d2679020974b
zoltan.gorbe <zoltan.gorbe@intland.com>
2019-03-22 08:45:21 +0100

    Menu improved but still needs refactoring

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+// Maybe the Menu component could be implemented as a function component with Hooks?
 class Menu extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+
+        this.intialLocation = this.props.location.pathname;
+    }
+    // this should be refactored to use state instead of DOM (css class) to store the active menu item
+    handleMenuClick = (e) => {
+        const menuItem = e.currentTarget,
+            menuList = menuItem.parentElement;
+            
+        for (let child of menuList.childNodes) {
+            child.classList.remove('active');
+        }
+
+        menuItem.classList.add('active');
+    }
+
+    // this looks ugly how it's called from jsx, should be refactored
+    getActiveClass = location => this.intialLocation === location ? 'active' : '';
+
     render() { 
         return ( 
             <nav className="menu p-col-12 p-md-6">
                 <ul>
-                    <li><a href="#/">Posts</a></li>
-                    <li><a href="#/contact">Contact</a></li>
-                    <li><a href="#/about">About</a></li>
+                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/') }><a href="#/">Posts</a></li>
+                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/contact') }><a href="#/contact">Contact</a></li>
+                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/about') }><a href="#/about">About</a></li>
                 </ul>
             </nav>
         );
     }
 }
  
-export default Menu;
+export default withRouter(Menu);
