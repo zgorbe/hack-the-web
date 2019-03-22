60d6fee85216cc5e448954054ce064c6990434ec
zoltan.gorbe <zoltan.gorbe@intland.com>
2019-03-22 17:19:43 +0100

    Listening for route change in Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,33 +3,31 @@ import { withRouter } from 'react-router-dom';
 
 // Maybe the Menu component could be implemented as a function component with Hooks?
 class Menu extends Component {
-    constructor(props) {
-        super(props);
-
-        this.intialLocation = this.props.location.pathname;
-    }
-    // this should be refactored to use state instead of DOM (css class) to store the active menu item
-    handleMenuClick = (e) => {
-        const menuItem = e.currentTarget,
-            menuList = menuItem.parentElement;
-            
-        for (let child of menuList.childNodes) {
-            child.classList.remove('active');
-        }
-
-        menuItem.classList.add('active');
+    state = {
+        location: this.props.location.pathname
     }
 
-    // this looks ugly how it's called from jsx, should be refactored
-    getActiveClass = location => this.intialLocation === location ? 'active' : '';
+    componentWillMount = () => {
+        this.unlisten = this.props.history.listen(location => {
+            this.setState({
+                location: location.pathname
+            });
+        });
+    }
+    componentWillUnmount = () => {
+        this.unlisten();
+    }
+
+    // it looks ugly how it's called from jsx, should be refactored
+    getActiveClass = location => this.state.location === location ? 'active' : '';
 
     render() { 
         return ( 
             <nav className="menu p-col-12 p-md-6">
                 <ul>
-                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/') }><a href="#/">Posts</a></li>
-                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/contact') }><a href="#/contact">Contact</a></li>
-                    <li onClick={ this.handleMenuClick } className={ this.getActiveClass('/about') }><a href="#/about">About</a></li>
+                    <li className={ this.getActiveClass('/') }><a href="#/">Posts</a></li>
+                    <li className={ this.getActiveClass('/contact') }><a href="#/contact">Contact</a></li>
+                    <li className={ this.getActiveClass('/about') }><a href="#/about">About</a></li>
                 </ul>
             </nav>
         );
