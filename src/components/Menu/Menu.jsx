099b43ce2d728e98c687c6850362c0e27cf59ebc
zoltan.gorbe <zoltan.gorbe@intland.com>
2019-03-25 08:44:49 +0100

    Menu is updated to use Hooks API

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,37 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-// Maybe the Menu component could be implemented as a function component with Hooks?
-class Menu extends Component {
-    state = {
-        location: this.props.location.pathname
-    }
+function Menu(props) {
+    const [location, setLocation] = useState(props.location.pathname);
 
-    componentWillMount = () => {
-        this.unlisten = this.props.history.listen(location => {
-            this.setState({
-                location: location.pathname
-            });
+    useEffect(() => {
+        const unlisten = props.history.listen(loc => {
+            setLocation(loc.pathname);
         });
-    }
-    componentWillUnmount = () => {
-        this.unlisten();
-    }
+        return function unsubscribe() {
+            unlisten();
+        };
+    });
 
-    // it looks ugly how it's called from jsx, should be refactored
-    getActiveClass = location => this.state.location === location ? 'active' : '';
-
-    render() { 
-        return ( 
-            <nav className="menu p-col-12 p-md-6">
-                <ul>
-                    <li className={ this.getActiveClass('/') }><a href="#/">Posts</a></li>
-                    <li className={ this.getActiveClass('/contact') }><a href="#/contact">Contact</a></li>
-                    <li className={ this.getActiveClass('/about') }><a href="#/about">About</a></li>
-                </ul>
-            </nav>
-        );
-    }
+    return ( 
+        <nav className="menu p-col-12 p-md-6">
+            <ul>
+                <li className={ location === '/' ? 'active' :  '' }><a href="#/">Posts</a></li>
+                <li className={ location === '/contact' ? 'active' :  ''}><a href="#/contact">Contact</a></li>
+                <li className={ location === '/about' ? 'active' :  '' }><a href="#/about">About</a></li>
+            </ul>
+        </nav>
+    );
 }
  
 export default withRouter(Menu);
